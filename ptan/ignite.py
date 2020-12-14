424b50569a4ec172ba49d9809a81d2fbad8fa2c3
Matt Lowell <lowell@lowelltech.com>
2020-12-14 12:03:15 +0100

    Add mean, std dev over last stat_size samples
    Track mean and standard deviation of reward and steps over the
    last stat_size samples, where stat_size is provided during
    object construction (default is 100).
    
    Add mean_reward, mean_steps, std_reward, and std_steps to state
    metrics dictionary.

--- ptan/ignite.py
+++ ptan/ignite.py
@@ -1,5 +1,6 @@
 import ptan
 import enum
+import numpy as np
 import time
 from typing import Optional
 from ignite.engine import Engine, State
@@ -16,19 +17,31 @@ class EpisodeEvents(EventEnum):
 class EndOfEpisodeHandler:
     def __init__(self, exp_source: ptan.experience.ExperienceSource, alpha: float = 0.98,
                  bound_avg_reward: Optional[float] = None,
+                 bound_mean_reward: Optional[float] = None,
+                 stat_size: int = 100,
                  subsample_end_of_episode: Optional[int] = None):
         """
         Construct end-of-episode event handler
         :param exp_source: experience source to use
         :param alpha: smoothing alpha param
         :param bound_avg_reward: optional boundary for average reward
+        :param bound_mean_reward: optional boundary for mean reward
+        :param stat_size: number of values for mean, std statistics; <= 1 to disable
         :param subsample_end_of_episode: if given, end of episode event will be subsampled by this amount
         """
         self._exp_source = exp_source
         self._alpha = alpha
         self._bound_avg_reward = bound_avg_reward
+        self._bound_mean_reward = bound_mean_reward
         self._best_avg_reward = None
         self._subsample_end_of_episode = subsample_end_of_episode
+        self._stat_size = stat_size
+        self._last_idx = 0
+        if self._stat_size > 1:
+            self._last_rewards = np.empty(self._stat_size, dtype=np.float32)
+            self._last_rewards[:] = np.NaN
+            self._last_steps = np.empty(self._stat_size, dtype=np.float32)
+            self._last_steps[:] = np.NaN
 
     def attach(self, engine: Engine):
         engine.add_event_handler(Events.ITERATION_COMPLETED, self)
@@ -44,10 +57,27 @@ class EndOfEpisodeHandler:
             engine.state.episode_steps = steps
             engine.state.metrics['reward'] = reward
             engine.state.metrics['steps'] = steps
+
+            if self._stat_size > 1:
+                # Keep stat_size elements in numpy arrays.
+                if (self._last_idx < 0) or (self._last_idx >= self._stat_size):
+                    self._last_idx = 0
+                self._last_rewards[self._last_idx] = reward
+                self._last_steps[self._last_idx] = steps
+                self._last_idx += 1
+
+                # Compute mean and standard deviation (nan* ignores NaN elements)
+                engine.state.metrics['mean_reward'] = np.nanmean(self._last_rewards)
+                engine.state.metrics['mean_steps'] = np.nanmean(self._last_steps)
+                engine.state.metrics['std_reward'] = np.nanstd(self._last_rewards)
+                engine.state.metrics['std_steps'] = np.nanstd(self._last_steps)
+
             self._update_smoothed_metrics(engine, reward, steps)
             if self._subsample_end_of_episode is None or engine.state.episode % self._subsample_end_of_episode == 0:
                 engine.fire_event(EpisodeEvents.EPISODE_COMPLETED)
             if self._bound_avg_reward is not None and engine.state.metrics['avg_reward'] >= self._bound_avg_reward:
+                engine.fire_event(EpisodeEvents.BOUND_REWARD_REACHED)
+            if self._bound_mean_reward is not None and self._stat_size >= 1 and engine.state.metrics['mean_reward'] >= self._bound_mean_reward:
                 engine.fire_event(EpisodeEvents.BOUND_REWARD_REACHED)
             if self._best_avg_reward is None:
                 self._best_avg_reward = engine.state.metrics['avg_reward']
